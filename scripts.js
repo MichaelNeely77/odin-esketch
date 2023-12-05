a60f73c9b3fd654701dbc350a05949b3a3de9354
MichaelNeely77 <michael@michaelneely.net>
2023-12-05 11:15:32 -0500

    Ran into first glitch with get getElementById not returning a value. Yay.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
-const app = document.getElementById('app');
+const rows=16;
+const columns = 16;
+
+const gridContainer = document.getElementById('esGrid');
+console.log(gridContainer);
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+        const gridItem = document.createElement('div');
+
+        gridItem.className = 'es-grid-item';
+        gridItem.textContent = `Cell ${i + 1}-${j + 1}`;
+        gridContainer.appendChild(gridItem);
+    }
+}
+
 
 
