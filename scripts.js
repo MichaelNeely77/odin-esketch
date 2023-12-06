5cd116fb9001955fb59535f05f822d2cc07dfe10
MichaelNeely77 <michael@michaelneely.net>
2023-12-06 13:39:54 -0500

    figured out the issue with getElementById as null

--- scripts.js
+++ scripts.js
@@ -1,17 +1,23 @@
 const rows=16;
 const columns = 16;
-
-const gridContainer = document.getElementById('esGrid');
+const gridContainer = document.getElementById("esGrid");
+const gridBorder = document.querySelector(".border");
+console.log(gridBorder);
 console.log(gridContainer);
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-        const gridItem = document.createElement('div');
 
-        gridItem.className = 'es-grid-item';
-        gridItem.textContent = `Cell ${i + 1}-${j + 1}`;
-        gridContainer.appendChild(gridItem);
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+
+
+            const gridItem = document.createElement("div");
+
+            gridItem.className = 'es-grid-item';
+            gridItem.textContent = `Cell ${i + 1}-${j + 1}`;
+            gridContainer.appendChild(gridItem);
+        }
     }
-}
+
+
 
 
 
