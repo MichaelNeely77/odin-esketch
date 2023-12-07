22e66717f467cd4a978812071f91ff71abf08c99
MichaelNeely77 <michael@michaelneely.net>
2023-12-07 09:32:25 -0500

    Working on event listener not working

--- scripts.js
+++ scripts.js
@@ -1,21 +1,28 @@
-const rows=16;
-const columns = 16;
+const rows= 64;
+const columns = 64;
 const gridContainer = document.getElementById("esGrid");
 const gridBorder = document.querySelector(".border");
 console.log(gridBorder);
 console.log(gridContainer);
 
+
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < columns; j++) {
 
-
             const gridItem = document.createElement("div");
-
             gridItem.className = 'es-grid-item';
 
             gridContainer.appendChild(gridItem);
         }
     }
+    const gridItemCell = document.querySelector(".es-grid-item");
+
+    gridItemCell.addEventListener('click', function(){
+        console.log("clicked");
+    });
+
+
+
 
 
 
