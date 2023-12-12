48cfb1b02487553cd6cc85ab72cd09e97f8d7be2
MichaelNeely77 <michael@michaelneely.net>
2023-12-12 10:13:35 -0500

    Added the function to change teh gridSize via prompt

--- scripts.js
+++ scripts.js
@@ -1,26 +1,34 @@
+const selectButton = document.querySelector(".grid-size-button button");
+
+function gridSizePrompt() {
+    let gridSize = prompt("How many squares would you like?");
+    let rows = gridSize;
+    let columns = gridSize;
+
+    for (let i = 0; i < rows; i++ ) {
+        for (let j = 0; j < columns; j++) {
+            const gridItem = document.createElement("div");
+            gridItem.className = "es-grid-item";
+            gridContainer.appendChild(gridItem);
+            gridItem.addEventListener("mouseover", () => {
+                gridItem.style.backgroundColor = "#ccc";
+            });
+            gridItem.addEventListener("dragenter", () => {
+                gridItem.style.backgroundColor = "#ccc";
+            });
+        }
+    }
+}
+selectButton.addEventListener('click', gridSizePrompt);
 
 
 
-const rows = 16;
-const columns = 16;
+
 
 const gridContainer = document.getElementById("es-grid-container");
 
 
-for (let i = 0; i < rows; i++ ) {
-    for (let j = 0; j < columns; j++) {
-        const gridItem = document.createElement("div");
-        gridItem.className = "es-grid-item";
-        gridContainer.appendChild(gridItem);
-        gridItem.addEventListener("mouseover", () => {
-            gridItem.style.backgroundColor = "#ccc";
-        });
-        gridItem.addEventListener("dragenter", () => {
-            gridItem.style.backgroundColor = "#ccc";
-        });
-    }
 
-}
 
 
 
