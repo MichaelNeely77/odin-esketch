0c9261196ce07cd4942b2edfebe9f75022c81a59
MichaelNeely77 <michael@michaelneely.net>
2023-12-17 11:04:51 -0500

    Fixed the prompt passing in the GridSize into the for loop then realized that I lost teh 16 x 16 grid on page load

--- scripts.js
+++ scripts.js
@@ -4,31 +4,31 @@
 // Create the divs using JavaScript - DONE
 //
 
-let rows=16;
-let columns = 16;
-const gridContainer = document.getElementById("es-grid-container");
+// let rows=16;
+// let columns = 16;
+// const gridContainer = document.getElementById("es-grid-container");
 
 
 
-const selectButton = document.querySelector(".grid-size-button button");
+// const selectButton = document.querySelector(".grid-size-button button");
 
-for (let i = 0; i < rows; i++ ) {
-    for (let j = 0; j < columns; j++) {
-        const gridItem = document.createElement("div");
-        gridItem.className = "es-grid-item";
-        gridContainer.appendChild(gridItem);
-        gridItem.addEventListener("mousedown", () => {
-            gridItem.style.backgroundColor = "#ccc";
-        });
-        // gridItem.addEventListener("mouseenter", () => {
-        //     gridItem.style.backgroundColor = "#ccc";
-        // });
-        gridItem.addEventListener("mouseup", () => {
-            gridItem.style.backgroundColor = "none";
-        });
-
-    }
-}
+// for (let i = 0; i < rows; i++ ) {
+//     for (let j = 0; j < columns; j++) {
+//         const gridItem = document.createElement("div");
+//         gridItem.className = "es-grid-item";
+//         gridContainer.appendChild(gridItem);
+//         gridItem.addEventListener("mousedown", () => {
+//             gridItem.style.backgroundColor = "#ccc";
+//         });
+//         // gridItem.addEventListener("mouseenter", () => {
+//         //     gridItem.style.backgroundColor = "#ccc";
+//         // });
+//         gridItem.addEventListener("mouseup", () => {
+//             gridItem.style.backgroundColor = "none";
+//         });
+//
+//     }
+// }
 
 // let gridSize = prompt("How many squares would you like?");
 // GridSize needs to be validated for specific limits
@@ -36,8 +36,42 @@ for (let i = 0; i < rows; i++ ) {
 // rows = gridSize;
 // columns = gridSize;
 
+
+
 /* Possible solution to making teh grid the same size is something like gridContainer.innerWidth / gridSize maybe?
 */
+
+let rows=16;
+let columns = 16;
+const selectBtn = document.querySelector(".grid-size-button button");
+
+const containerWidth = document.getElementById('es-grid-container').clientWidth;
+
+const gridContainer = document.getElementById("es-grid-container");
+selectBtn.addEventListener('click', () => {
+    let gridSize = prompt("How many squares would you like?");
+    rows = gridSize;
+    columns = gridSize;
+    console.log(gridSize);
+    let totalSquares = rows * columns;
+    for(let i = 0; i < totalSquares; i++) {
+        const gridItemSize = containerWidth / rows;
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('es-grid-item');
+        gridItem.style.width = `${gridItemSize}px`;
+        gridItem.style.height = `${gridItemSize}px`;
+        gridContainer.appendChild(gridItem);
+    }
+});
+
+
+
+
+
+
+
+
+
 
 
 
